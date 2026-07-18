a9b672fcfbd93631d2bd6c338d102cea21381b17
Ana Clarissi <anaclarissicruz@gmail.com>
2026-07-18 12:43:48 -0300

    feat: finish mobile design with dark and light theme

--- src/js/background.js
+++ src/js/background.js
@@ -1,3 +1,13 @@
 const switchToggle = document.querySelector("#switch");
 
-switchToggle.addEventListener("input", () => console.log("Hello World"));
+switchToggle.addEventListener("input", () => toggleTheme());
+
+function toggleTheme() {
+
+    const theme = switchToggle.checked ? "light" : "dark";
+
+    const body = document.querySelector("body");
+
+    body.setAttribute("data-theme", theme);
+
+}
